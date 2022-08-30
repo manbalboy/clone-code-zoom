32c2b7ad04d50a314a3b07bd832d6d45a07c23ab
manbalboy <manbalboy@hanmail.net>
2022-08-30 21:12:58 +0900

    RTC를 이용한 peer to peer 통신으로 웹 화상 채팅 구현

--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 import http from 'http';
-import {Server} from 'socket.io';
-import {instrument} from '@socket.io/admin-ui';
+import SocketIO from 'socket.io';
 import express from 'express';
-import { doesNotMatch } from 'assert';
 
 const app = express();
 
@@ -13,68 +11,24 @@ app.get('/', (req, res) => res.render('home'));
 app.get('/*', (req, res) => res.redirect('/'));
 
 const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer, {
-	cors : {
-		origin: ['https://admin.socket.io'],
-		credentials: true
-	}
-});
-
-instrument(wsServer, {
-	auth: false,
-});
-
-function publicRooms() {
-	const {
-		sockets: {
-			adapter: { sids, rooms },
-		},
-	} = wsServer;
-
-	const publicRoomsArr = [];
-	rooms.forEach((_, key) => {
-		if (sids.get(key) === undefined) {
-			publicRoomsArr.push(key);
-		}
-	});
-	return publicRoomsArr;
-}
-
-function countRoom(roomName) {
-	return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
+const wsServer = SocketIO(httpServer);
 
 wsServer.on('connection', socket => {
-	socket.nickname = 'Anon';
-	socket.onAny(event => {
-		console.log(wsServer.sockets.adapter);
-		console.log(`Socket Event: ${event}`);
-	});
-	socket.on('enter_room', (roomName, done) => {
-		done();
-		console.log(roomName);
+	socket.on('join_room', roomName => {
 		socket.join(roomName);
-		socket.to(roomName).emit('welcome', socket.nickname, countRoom(roomName));
-		wsServer.sockets.emit('room_change', publicRooms());
+		socket.to(roomName).emit('welcome');
 	});
 
-	socket.on('disconnecting', () => {
-		socket.rooms.forEach(room => {
-			socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1);
-		});
+	socket.on('offer', (offer, roomName) => {
+		socket.to(roomName).emit('offer', offer);
 	});
 
-	socket.on('disconnect', () => {
-		wsServer.sockets.emit('room_change', publicRooms());
+	socket.on('answer', (answer, roomName) => {
+		socket.to(roomName).emit('answer', answer);
 	});
 
-	socket.on('new_message', (msg, room, done) => {
-		socket.to(room).emit('new_message', `${socket.nickname}: ${msg}`);
-		done();
-	});
-
-	socket.on('nickname', nickname => {
-		socket.nickname = nickname;
+	socket.on('ice', (ice, roomName) => {
+		socket.to(roomName).emit('ice', ice);
 	});
 });
 
